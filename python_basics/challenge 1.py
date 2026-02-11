1404c46336ce78280d3111f8942cc980b26edfa1
marjoriemukami-cyber <marjorie.mukami@gmail.com>
2026-02-11 09:17:07 +0300

    first commit

---
+++ python_basics/challenge 1.py
@@ -0,0 +1,9 @@
+university = "CUEA"
+grade = "B+"
+favourite_course = ""architecture"
+cluster_points = "39"
+
+print(f"My favourite university is{university}")
+print(f"My KCSE grade is {grade}")
+print(f"My favourite course is{favourite_course}")
+print(f"My cluster points are{cluster_points}")
---
+++ python_basics/hello.py
@@ -0,0 +1,13 @@
+
+print("hello from Marjorie")
+print("I am from Juja")
+
+print("I like to bake,listen to music,playing the piano,writing poems")
+
+age = 17
+height =1.63
+weight = 68
+
+print(f"I am {age} years old")
+print(f"I am also {height} metres tall")
+print(f"I weigh {weight} kgs")
