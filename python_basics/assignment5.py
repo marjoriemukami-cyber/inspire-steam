78fd08cc6072f7af9a47f9deb2ad2d639e38c95c
marjorie <marjorie.mukami@gmail.com>
2026-02-16 20:15:48 +0300

    Program to calculate income tax assignment

--- python_basics/assignment5.py
+++ python_basics/assignment5.py
@@ -2,12 +2,32 @@
 # Date : 16/02/2026
 # Program to calculate income tax
 
-salary = int(input("Enter your gross salary"))
 
-if salary < 50000:
+
+# salary < 50000: 
+if salary >= 50000 and salary <= 100000:
     tax = (2.5 * salary) / 100
     net_salary = salary - tax
+print(f"Gross salary = {salary}")
+print(f"Net salary = {net_salary}")
+print(f"Tax = {tax}")
+
+
+salary = int(input("Enter your gross salary"))
+
+tax = 0
+net_salary = (int)salary
+
+if salary >= 50000 and salary <= 100000:
+    tax = (4.5 * salary) / 100
+    net_salary = salary - tax
+
+elif salary > 100000:
+    tax = (5.0 * salary) / 100
+    net_salary = salary - tax  
+
 
 print(f"Gross salary = {salary}")
 print(f"Net salary = {net_salary}")
 print(f"Tax = {tax}")
+
---
+++ python_basics/assignment5a.py
@@ -0,0 +1,19 @@
+# Name : Marjorie Mukami
+# Date : 16/02/2026
+# Program to calculate income tax
+
+user_input = input("Enter your gross salary: ")
+salary = int(user_input)
+
+tax = 0.0
+net_salary = float(salary)
+
+
+if 50000 <= salary <= 100000:
+    tax = (4.5 * salary) / 100
+    net_salary = salary - tax
+
+
+print(f"Gross salary = {salary}")
+print(f"Net salary = {net_salary}")
+print(f"Tax = {tax}")
