aecc7b69abc74b77df5443875251c16644e023ab
marjorie <marjorie.mukami@gmail.com>
2026-02-20 08:43:41 +0300

    Program to create game using functions

--- python_basics/assignment7.py
+++ python_basics/assignment7.py
@@ -42,3 +42,7 @@ ninja_character.kick(warrior_character)
 
 ninja_character.report_status()
 warrior_character.report_status()
+
+warrior_character.takle(ninja_character)
+ninja_character.report_status()
+warrior_character.report_status()
